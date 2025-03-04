cb90e247ddebe1cd7b65ed1751cb568ae83526a4
TuanHo10102003 <anhtuan6563@gmail.com>
2025-03-04 20:41:14 +0700

    TuanHo-4/3/2025

--- WebTest/Areas/Admin/Controllers/HomeController.cs
+++ WebTest/Areas/Admin/Controllers/HomeController.cs
@@ -13,6 +13,17 @@ namespace WebTest.Areas.Admin.Controllers
         // GET: Admin/Home
         public ActionResult Index()
         {
+            ViewBag.HomNay = HttpContext.Application["HomNay"];
+            ViewBag.HomQua = HttpContext.Application["HomQua"];
+            ViewBag.TuanNay = HttpContext.Application["TuanNay"];
+            ViewBag.TuanTruoc = HttpContext.Application["TuanTruoc"];
+            ViewBag.ThangNay = HttpContext.Application["ThangNay"];
+            ViewBag.ThangTruoc = HttpContext.Application["ThangTruoc"];
+            ViewBag.TatCa = HttpContext.Application["TatCa"];
+            ViewBag.VisitorsOnline = HttpContext.Application["visitors_online"];
+
+
+
             return View();
         }
     }
